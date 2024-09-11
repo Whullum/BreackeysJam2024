287d7e2b0d894ad6793f3f98afb5a6aaff6aadb6
octia <74676459+octia@users.noreply.github.com>
2024-09-11 20:24:42 +0200

    Add sound manager for Will

--- Assets/_Scripts/Infrastructure/ProjectInstaller.cs
+++ Assets/_Scripts/Infrastructure/ProjectInstaller.cs
@@ -1,12 +1,15 @@
 using Zenject;
+using UnityEngine;
 
 namespace _Scripts.Infrastructure
 {
     public class ProjectInstaller : MonoInstaller
     {
+        [SerializeField]
+        private SoundManager _soundManager;
         public override void InstallBindings()
         {
-            
+            Container.Bind<SoundManager>().FromInstance(_soundManager).AsSingle();
         }
     }
 }
