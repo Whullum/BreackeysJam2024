79a7e146c014b380173f20c6fa2cb16ba71d2e73
GarrettD33 <gdavids1992@gmail.com>
2024-09-14 17:19:44 -0400

    Remove Cursor Change Outline
    Removed cursor mouseover effect and attempted to change outline mouseover effect from sprite swap to Outline change. Unsuccessful. Pushing for Octia to Review.

--- Assets/_Scripts/UI/Gameplay/ButtonHovers/SimButtonHover.cs
+++ Assets/_Scripts/UI/Gameplay/ButtonHovers/SimButtonHover.cs
@@ -9,23 +9,23 @@ using UnityEngine.UI;
 public class SimButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
 {
-    public Texture2D cursorTexture;
-    public CursorMode cursorMode = CursorMode.Auto;
-    public Vector2 hotSpot = Vector2.zero;
-    public Sprite originalSprite;
-    public Sprite highlightSprite;
+    [SerializeField] Color _highlightColor;
+    [SerializeField] Color _originalColor;
+    private Outline _outline;
 
+    private void Awake() 
+    {
+        _outline = GetComponent<Outline>();
+    }
     
     public void OnPointerEnter(PointerEventData _)
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
-        GetComponent<Image>().sprite = highlightSprite;
+        _outline.effectColor = _highlightColor;
         
     }
 
     public void OnPointerExit(PointerEventData _)
     {
-        Cursor.SetCursor(null, Vector2.zero, cursorMode);
-        GetComponent<Image>().sprite = originalSprite;
+        _outline.effectColor = _originalColor;
     }
 }
