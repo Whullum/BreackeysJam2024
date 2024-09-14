a5c3877c7fd286ab2f70b3d857ea33374b3b3066
GarrettD33 <gdavids1992@gmail.com>
2024-09-14 10:13:45 -0400

    SimButtonHoverOutline
    Change outline color of sim button on mouseover

--- Assets/_Scripts/UI/Gameplay/ButtonHovers/SimButtonHover.cs
+++ Assets/_Scripts/UI/Gameplay/ButtonHovers/SimButtonHover.cs
@@ -4,6 +4,7 @@ using DG.Tweening;
 using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SimButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
@@ -11,14 +12,20 @@ public class SimButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     public Texture2D cursorTexture;
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
+    public Sprite originalSprite;
+    public Sprite highlightSprite;
 
+    
     public void OnPointerEnter(PointerEventData _)
     {
         Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        GetComponent<Image>().sprite = highlightSprite;
+        
     }
 
     public void OnPointerExit(PointerEventData _)
     {
         Cursor.SetCursor(null, Vector2.zero, cursorMode);
+        GetComponent<Image>().sprite = originalSprite;
     }
 }
