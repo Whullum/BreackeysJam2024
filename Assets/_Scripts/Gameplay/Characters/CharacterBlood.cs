573d079ff84248fabb0fb4cee05362807216086a
nyarla <nyarladev@gmail.com>
2024-09-15 11:44:38 +0300

    BLOOOD!

---
+++ Assets/_Scripts/Gameplay/Characters/CharacterBlood.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace _Scripts.Gameplay.Characters
+{
+    public class CharacterBlood : MonoBehaviour
+    {
+        [SerializeField]
+        private ParticleSystem _particleSystem;
+        
+        [SerializeField]
+        private float _bloodDuration;
+
+        [SerializeField]
+        private CharacterLife _model;
+
+        private void Awake()
+        {
+            _model.TookHit += DropBlood;
+        }
+
+        public void DropBlood()
+        {
+            _particleSystem.Play();
+        }
+    }
+}
